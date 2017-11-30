becdd6243d4937e6171acc807965c40d50f7757f
燎 石橋 <150461@jc-21.jp>
2017-11-30 14:39:22 +0900

    初回

--- BMI/BMI/MainPage.xaml.cs
+++ BMI/BMI/MainPage.xaml.cs
@@ -13,5 +13,45 @@ namespace BMI
         {
             InitializeComponent();
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Double w = Double.Parse(weight.Text);
+
+            Double h = Double.Parse(height.Text);
+
+            if (h >= 3)
+            {
+                h /= 100;
+            }
+
+            double bmi = w / h / h;
+
+            if (bmi < 18.5)
+            {
+                DisplayAlert("BMI", bmi2.ToString() + "低体重(痩せ型)です", "OK");
+            }
+            else if (bmi < 25)
+            {
+                DisplayAlert("BMI", bmi2.ToString() + "普通体重です", "OK");
+            }
+            else if (bmi < 30)
+            {
+                DisplayAlert("BMI", bmi2.ToString() + "肥満(1度)です", "OK");
+            }
+            else if (bmi < 35)
+            {
+                DisplayAlert("BMI", bmi2.ToString() + "肥満(2度)です", "OK");
+            }
+            else if (bmi < 40)
+            {
+                DisplayAlert("BMI", bmi2.ToString() + "肥満(3度)です", "OK");
+            }
+            else
+            {
+                DisplayAlert("BMI", bmi2.ToString() + "肥満(4度)です", "OK");
+            }
+
+        }
     }
 }
